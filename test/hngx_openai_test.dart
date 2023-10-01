e6a1919f083306b874cf8fb093a14a5e3c031150
eokdev <emmanuelgbenga14@gmail.com>
2023-10-01 15:46:20 +0100

    .

--- test/hngx_openai_test.dart
+++ test/hngx_openai_test.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:hngx_openai/hngx_openai.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:hngx_openai/hngx_openai.dart';
 
-void main() {
-  test('Returns HELLO in lower case', () {
-    final openAI = OpenAI();
-    expect(openAI.sample("HELLO"), 'hello');
-  });
-}
+// void main() {
+//   test('Returns HELLO in lower case', () {
+//     final openAI = OpenAI();
+//     expect(openAI.sample("HELLO"), 'hello');
+//   });
+// }

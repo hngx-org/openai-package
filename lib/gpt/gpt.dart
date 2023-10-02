9407af8a878eca569b5640d9844694c8488771cd
eokdev <emmanuelgbenga14@gmail.com>
2023-10-02 10:07:12 +0100

    gpt

---
+++ lib/gpt/gpt.dart
@@ -0,0 +1,17 @@
+library gpt;
+
+// import 'package:dotenv/dotenv.dart' as dotenv;
+import 'package:dotenv/dotenv.dart';
+
+DotEnv env = DotEnv(includePlatformEnvironment: true)..load();
+
+/// Globally access the API key, import in any file needed
+String? apiKey = env['OPENAI_API_KEY'];
+
+/// Globally access the baseUrl for completions, import in any file needed
+String? baseUrlcompletions = env['OPENAI_BASE_URL'];
+
+class GPT {
+  /// Returns [name] of this model.
+  String get apiName => "GPT API";
+}

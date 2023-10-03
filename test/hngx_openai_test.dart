5ef1260e9a2e02a7bb2c114b6633fddc9700ea66
Farouk Bello <mhiztahymodder@gmail.com>
2023-10-03 19:18:41 +0100

    t

--- test/hngx_openai_test.dart
+++ test/hngx_openai_test.dart
@@ -1,5 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hngx_openai/repository/openai_repository.dart';
 
 void main() {
-  test('Checking if our endpoint works', () async {});
+  test("Checking if we're getting response from the server", () async {
+    const String userInput = "What is today's date";
+    const String cookie =
+        "session=487d97a5-3e43-4502-80d4-9315c3d7bf77.24ZfCu95q06BqVuCUFWuJJoLAgM";
+
+    final response = await OpenAIRepository().getChat(userInput, cookie);
+    expect(response.isNotEmpty, true);
+  });
 }

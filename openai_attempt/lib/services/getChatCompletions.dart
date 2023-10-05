3243a2c02a616f572895418fd36826516e74a3c9
eokdev <emmanuelgbenga14@gmail.com>
2023-10-05 20:20:44 +0100

    new

---
+++ openai_attempt/lib/services/getChatCompletions.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hngx_openai/repository/openai_repository.dart';
+
+class ChatCompletionServices {
+  final ProviderRef ref;
+  ChatCompletionServices(this.ref);
+  Future<ResModel> chatCompletion({required GetChatCompletionDataModel model}) async {
+    try {
+      final aiResponseC = await OpenAIRepository().getChatCompletions(
+        model.history!,
+        model.userInput!,
+        model.cookies!,
+      );
+      if (aiResponseC.startsWith("M")) {
+        return ResModel(
+          status: "Success",
+          errMessage: null,
+        );
+      } else {
+        return ResModel(
+          status: "Failed",
+          errMessage: aiResponseC,
+        );
+      }
+    } catch (e) {
+      return ResModel(status: "failed", errMessage: "An error occured");
+    }
+  }
+}
+
+class ResModel {
+  final String? status;
+  final String? errMessage;
+
+  ResModel({this.status, this.errMessage});
+}
+
+final chatCompletionProvider = Provider<ChatCompletionServices>((ref) => ChatCompletionServices(ref));
+final chatCompletionResponse = StateProvider.autoDispose<ResModel?>((ref) => null);
+
+final chatCompletion = FutureProvider.autoDispose.family<bool, GetChatCompletionDataModel>((ref, arg) async {
+  final fetchdata = await ref.read(chatCompletionProvider).chatCompletion(model: arg);
+  final isAuth = fetchdata.status == "Success";
+  if (isAuth) {
+    ref.read(chatCompletionResponse.notifier).state = fetchdata;
+  } else {
+    ref.read(chatCompletionResponse.notifier).state = fetchdata;
+  }
+  return true;
+});
+
+class GetChatCompletionDataModel {
+  final List<String>? history;
+  final String? userInput;
+  final String? cookies;
+
+  GetChatCompletionDataModel({required this.history, required this.userInput, required this.cookies});
+}

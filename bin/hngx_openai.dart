5e85da6b8e3330dbf191e9b75c48ec923d2e649e
Farouk Bello <mhiztahymodder@gmail.com>
2023-10-05 15:09:47 +0100

    sample app initiated...

--- bin/hngx_openai.dart
+++ bin/hngx_openai.dart
@@ -7,11 +7,24 @@ import 'package:hngx_openai/repository/openai_repository.dart';
 void main() async {
   const String userInput = "What is today's date";
   const String cookie =
-      "session=96ef3dbc-90ae-4bc0-9c59-812d2155585b.U0PxaqhJkcUjpbLiL7mojeTXXOc";
+      "session=8b611d1b-d438-4755-90e8-d3ff0610baa1.AL8Tvg99Y7hbKMnHkiYKqA8-kso";
 
   // For getting chat
   // final aiResponse = await OpenAIRepository().getChat(userInput, cookie);
   // For chat completions
   final aiResponseC = await OpenAIRepository().getChat(userInput, cookie);
+
   log(aiResponseC);
+}
+
+String filterText(String response) {
+  if (response.startsWith('M')) {
+    // If the return String is a Message
+    log("This is a Success Text");
+    return response.substring(8).trim();
+  } else {
+    // If the return String is an Error
+    log("This is an Error Text");
+    return response.substring(6).trim();
+  }
 }
